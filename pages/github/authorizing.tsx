b4787a10b1411ce2faf84840f0078c10dfb63fd3
Rupert Minnett <rminnett@gmail.com>
2020-06-25 22:49:35 -0700

    Formatting

--- pages/github/authorizing.tsx
+++ pages/github/authorizing.tsx
@@ -2,10 +2,8 @@ import { useGithubAuthRedirect } from 'react-tinacms-github'
 
 // GitHub redirects back to this page with the auth code
 export default function Authorizing() {
-
   // Report that an auth code was received from GitHub
   useGithubAuthRedirect()
 
   return <h2>Authorizing with GitHub, please wait...</h2>
-
 }
