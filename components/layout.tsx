16305589b5dbfb77f12cc535bab963be55fa2d0e
Rupert Minnett <rminnett@gmail.com>
2020-07-01 23:16:51 -0700

    Add branch-switcher

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,104 +1,103 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import React, { FunctionComponent } from 'react'
-import { Menu, Container, Button } from 'semantic-ui-react'
-import { useCMS } from 'tinacms'
+import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { FunctionComponent } from 'react';
+import { Menu, Container, Button } from 'semantic-ui-react';
+import { useCMS } from 'tinacms';
 
 const Layout: FunctionComponent = ({ children }) => {
-  const router = useRouter()
-  const cms = useCMS()
-  return (
-    <>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <Container>{children}</Container>
-      </main>
-      <header>
-        <Menu pointing secondary inverted>
-          <Container>
-            <img className="logo" src="/logo.png" />
-            <Link href="/">
-              <Menu.Item
-                active={router.pathname === '/'}
-                as="a"
-                name="home"
-                style={{ margin: '-.5em 0 .5em' }}
-              >
-                Home
-              </Menu.Item>
-            </Link>
-            <Link href="/about-us">
-              <Menu.Item
-                active={router.pathname === '/about-us'}
-                as="a"
-                name="about-us"
-                style={{ margin: '-.5em 0 .5em' }}
-              >
-                About Us
-              </Menu.Item>
-            </Link>
-            <Menu.Item position="right">
-              <Button as="a" inverted onClick={() => cms && cms.toggle()}>
-                {cms && cms.enabled ? 'Exit Edit Mode' : 'Edit Mode'}
-              </Button>
-            </Menu.Item>
-          </Container>
-        </Menu>
-      </header>
-      <footer>
-        <Container>Footer</Container>
-      </footer>
-      <style jsx>{`
-        header {
-          position: fixed;
-          width: 100%;
-          background: black;
-          padding: 0.5em 0;
-        }
-        .logo {
-          height: 3em;
-          padding-right: 1em;
-          margin-right: 1em;
-          border-right: 1px solid white;
-        }
-        header .menuItem {
-          margin-top: -0.5em;
-          margin-bottom: 0.5em;
-        }
-        main {
-          position: absolute;
-          width: 100%;
-          padding-top: 6em;
-        }
-        footer {
-          position: absolute;
-          bottom: 0;
-          width: 100%;
-          height: 60px;
-          padding-top: 20px;
-          padding-bottom: 20px;
-          background-color: #f5f5f5;
-          margin-top: 0;
-          margin-bottom: 0;
-        }
-      `}</style>
+	const router = useRouter();
+	const cms = useCMS();
+	return (
+		<>
+			<Head>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<main>
+				<Container>{children}</Container>
+			</main>
+			<header>
+				<Menu pointing secondary inverted>
+					<Container>
+						<img height='42' width='147' className='logo' src='/logo.png' />
+						<Link href='/'>
+							<Menu.Item
+								active={router.pathname === '/'}
+								as='a'
+								name='home'
+								style={{ margin: '-.5em 0 .5em' }}
+							>
+								Home
+							</Menu.Item>
+						</Link>
+						<Link href='/about-us'>
+							<Menu.Item
+								active={router.pathname === '/about-us'}
+								as='a'
+								name='about-us'
+								style={{ margin: '-.5em 0 .5em' }}
+							>
+								About Us
+							</Menu.Item>
+						</Link>
+						<Menu.Item position='right'>
+							<Button as='a' inverted onClick={() => cms && cms.toggle()}>
+								{cms && cms.enabled ? 'Exit Edit Mode' : 'Edit Mode'}
+							</Button>
+						</Menu.Item>
+					</Container>
+				</Menu>
+			</header>
+			<footer>
+				<Container>Footer</Container>
+			</footer>
+			<style jsx>{`
+				header {
+					position: fixed;
+					width: 100%;
+					background: black;
+					padding: 0.5em 0;
+				}
+				.logo {
+					padding-right: 1em;
+					margin-right: 1em;
+					border-right: 1px solid white;
+				}
+				header .menuItem {
+					margin-top: -0.5em;
+					margin-bottom: 0.5em;
+				}
+				main {
+					position: absolute;
+					width: 100%;
+					padding-top: 6em;
+				}
+				footer {
+					position: absolute;
+					bottom: 0;
+					width: 100%;
+					height: 60px;
+					padding-top: 20px;
+					padding-bottom: 20px;
+					background-color: #f5f5f5;
+					margin-top: 0;
+					margin-bottom: 0;
+				}
+			`}</style>
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-        }
+			<style jsx global>{`
+				html,
+				body {
+					padding: 0;
+					margin: 0;
+				}
 
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </>
-  )
-}
+				* {
+					box-sizing: border-box;
+				}
+			`}</style>
+		</>
+	);
+};
 
-export default Layout
+export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,66 +1,66 @@
-import App, { AppProps } from 'next/app'
-import React, { FunctionComponent } from 'react'
-import { RecoilRoot } from 'recoil'
-import { TinaCMS, TinaProvider } from 'tinacms'
-import { GithubClient, TinacmsGithubProvider } from 'react-tinacms-github'
-import 'semantic-ui-css/semantic.min.css'
-
-const AppComponent: FunctionComponent<{ pageProps?: any }> = ({
-  pageProps,
-  children,
-}) => {
-  const cms = new TinaCMS({
-    enabled: pageProps.preview,
-    apis: {
-      github: new GithubClient({
-        proxy: '/api/proxy-github',
-        authCallbackRoute: '/api/create-github-access-token',
-        clientId: process.env.GITHUB_CLIENT_ID,
-        baseRepoFullName: process.env.REPO_FULL_NAME,
-        baseBranch: process.env.BASE_BRANCH,
-      }),
-    },
-    sidebar: true,
-    toolbar: true,
-  })
-  return (
-    <TinaProvider cms={cms}>
-      <TinacmsGithubProvider
-        onLogin={() => {
-          const token = localStorage.getItem('tinacms-github-token') || null
-          const headers = new Headers()
-          if (token) {
-            headers.append('Authorization', 'Bearer ' + token)
-          }
-          return fetch(`/api/preview`, { headers: headers }).then(() => {
-            window.location.href = window.location.pathname
-          })
-        }}
-        onLogout={() => {
-          return fetch(`/api/reset-preview`).then(() => {
-            window.location.reload()
-          })
-        }}
-        error={pageProps.error}
-      >
-        {children}
-      </TinacmsGithubProvider>
-    </TinaProvider>
-  )
-}
+import App, { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
+import { TinaCMS, TinaProvider } from 'tinacms';
+import { GithubClient, TinacmsGithubProvider } from 'react-tinacms-github';
+import { BranchSwitcherPlugin } from '../tina-plugins/branch-switcher';
+import 'semantic-ui-css/semantic.min.css';
 
 export default class AppClass extends App {
-  constructor(props: AppProps) {
-    super(props)
-  }
-  render(): JSX.Element {
-    const { Component, pageProps } = this.props
-    return (
-      <RecoilRoot>
-        <AppComponent pageProps={pageProps}>
-          <Component {...pageProps} />
-        </AppComponent>
-      </RecoilRoot>
-    )
-  }
+	cms: TinaCMS;
+	constructor(props: AppProps) {
+		super(props);
+		this.cms = new TinaCMS({
+			enabled: props.pageProps.preview,
+			plugins: [BranchSwitcherPlugin],
+			apis: {
+				github: new GithubClient({
+					proxy: '/api/proxy-github',
+					authCallbackRoute: '/api/create-github-access-token',
+					clientId: process.env.GITHUB_CLIENT_ID,
+					baseRepoFullName: process.env.REPO_FULL_NAME,
+					baseBranch: process.env.BASE_BRANCH,
+				}),
+			},
+			sidebar: true,
+			toolbar: {
+				buttons: {
+					save: 'Save',
+					reset: 'Reset',
+				},
+			},
+		});
+	}
+	render(): JSX.Element {
+		const { Component, pageProps } = this.props;
+		return (
+			<TinaProvider cms={this.cms}>
+				<TinacmsGithubProvider
+					onLogin={onLogin}
+					onLogout={onLogout}
+					error={pageProps.error}
+				>
+					<RecoilRoot>
+						<Component {...pageProps} />
+					</RecoilRoot>
+				</TinacmsGithubProvider>
+			</TinaProvider>
+		);
+	}
 }
+
+const onLogin = () => {
+	const token = localStorage.getItem('tinacms-github-token') || null;
+	const headers = new Headers();
+	if (token) {
+		headers.append('Authorization', 'Bearer ' + token);
+	}
+	return fetch(`/api/preview`, { headers: headers }).then(() => {
+		window.location.href = window.location.pathname;
+	});
+};
+
+const onLogout = () => {
+	return fetch(`/api/reset-preview`).then(() => {
+		window.location.reload();
+	});
+};
